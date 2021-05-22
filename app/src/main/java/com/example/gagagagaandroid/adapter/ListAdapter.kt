95dc35b0c641416a4455d945913fbcc559fe9c75
kmebin <blowhuiee@gmail.com>
2021-05-23 06:36:45 +0900

    [add] 리스트뷰 추가

---
+++ app/src/main/java/com/example/gagagagaandroid/adapter/ListAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.gagagagaandroid.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.gagagagaandroid.ListClickListener
+import com.example.gagagagaandroid.databinding.ListItemBinding
+import com.example.gagagagaandroid.ui.list.ListData
+
+class ListAdapter(
+    private val itemClick: ItemClick
+): RecyclerView.Adapter<ListAdapter.ListViewHolder>() {
+    private var dataList = mutableListOf<ListData>()
+
+    inner class ListViewHolder(private val binding: ListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun onBind(list: ListData) {
+            Glide.with(binding.root).load(list.image).into(binding.profileImg)
+            binding.name.text = list.name
+            binding.description.text = list.description
+            binding.tag1.text = list.tags[0]
+            binding.tag2.text = list.tags[0]
+            binding.tag3.text = list.tags[0]
+            binding.root.setOnClickListener { itemClick.onClick() }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val binding = ListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        holder.onBind(dataList[position])
+    }
+
+    override fun getItemCount() = dataList.size
+
+    fun replaceList(newList: MutableList<ListData>) {
+        dataList = newList.toMutableList()
+        notifyDataSetChanged()
+    }
+
+    interface ItemClick {
+        fun onClick()
+    }
+}
--- app/src/main/java/com/example/gagagagaandroid/adapter/MainViewPagerAdapter.kt
+++ app/src/main/java/com/example/gagagagaandroid/adapter/MainViewPagerAdapter.kt
@@ -3,8 +3,8 @@ package com.example.gagagagaandroid.adapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.example.gagagagaandroid.ui.SecondFragment
-import com.example.gagagagaandroid.ui.ThirdFragment
+import com.example.gagagagaandroid.ui.home.HomeFragment
+import com.example.gagagagaandroid.ui.list.ListFragment
 import com.example.gagagagaandroid.ui.first.FirstFragment
 
 class MainViewPagerAdapter(fragmentActivity: FragmentActivity) :
@@ -14,8 +14,8 @@ class MainViewPagerAdapter(fragmentActivity: FragmentActivity) :
     override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> FirstFragment()
-            1 -> SecondFragment()
-            else -> ThirdFragment()
+            1 -> HomeFragment()
+            else -> ListFragment()
         }
     }
 }
