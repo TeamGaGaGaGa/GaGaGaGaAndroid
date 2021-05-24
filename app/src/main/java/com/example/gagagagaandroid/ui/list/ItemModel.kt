623c2b7b6c6645540f7f2738aae5e2aa85208194
kmebin <blowhuiee@gmail.com>
2021-05-25 01:12:34 +0900

    [update] colors 및 themes 수정

--- app/src/main/java/com/example/gagagagaandroid/ui/list/ItemModel.kt
+++
@@ -1,5 +0,0 @@
-package com.example.gagagagaandroid.ui.list
-
-data class ItemModel (
-    val isClick: Boolean
-)
--- app/src/main/java/com/example/gagagagaandroid/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/gagagagaandroid/ui/main/MainActivity.kt
@@ -5,10 +5,8 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.viewpager2.widget.ViewPager2
 import com.example.gagagagaandroid.R
-import com.example.gagagagaandroid.adapter.HomeListAdapter
 import com.example.gagagagaandroid.adapter.MainViewPagerAdapter
 import com.example.gagagagaandroid.databinding.ActivityMainBinding
-import com.example.gagagagaandroid.ui.homefragment.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -19,16 +17,12 @@ class MainActivity : AppCompatActivity() {
         setViewPagerAdapter()
         binding.bnvMain.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.item1 -> binding.vpMain.currentItem = 0
-                R.id.item2 -> binding.vpMain.currentItem = 1
+                R.id.add -> binding.vpMain.currentItem = 0
+                R.id.home -> binding.vpMain.currentItem = 1
                 else -> binding.vpMain.currentItem = 2
             }
             true
         }
-//        val homeListFragment = HomeFragment()
-//        val transaction = supportFragmentManager.beginTransaction()
-//        transaction.add(R.id.homeList_container, homeListFragment)
-//        transaction.commit()
     }
 
     private fun setViewPagerAdapter() {
@@ -44,9 +38,9 @@ class MainActivity : AppCompatActivity() {
         override fun onPageSelected(position: Int) {
             super.onPageSelected(position)
             binding.bnvMain.selectedItemId = when (position) {
-                0 -> R.id.item1
-                1 -> R.id.item2
-                2 -> R.id.item3
+                0 -> R.id.add
+                1 -> R.id.home
+                2 -> R.id.list
                 else -> throw IllegalArgumentException("Wrong Position $position")
             }
         }
