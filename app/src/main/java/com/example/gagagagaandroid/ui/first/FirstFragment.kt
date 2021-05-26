bae31d4b9420fe1d79d1c848711fc89b157b9b9d
Leekangmin <kkk5474096@naver.com>
2021-05-26 17:03:32 +0900

    [update] FirstFragment 리스너 -> ChipGroup 변경

--- app/src/main/java/com/example/gagagagaandroid/ui/first/FirstFragment.kt
+++ app/src/main/java/com/example/gagagagaandroid/ui/first/FirstFragment.kt
@@ -49,11 +49,11 @@ class FirstFragment : Fragment() {
             binding.tvPartMain.text = "기획 파트"
             binding.tvPartMainQuestion.text = "뭐라꼬? \n니 기획자가?"
             binding.tvPartQuestion1.text = "기획에는 TJ만 있을거 같죠? \n얼마나 계획적인 사람일까요?"
-            binding.btnPartCheckYes.text = "계획하는일이 세상에서 제일 재밌어!"
-            binding.btnPartCheckNo.text = "계획? 나는 그런거 몰라유"
+            binding.chipPartCheckYes.text = "계획하는일이 세상에서 제일 재밌어!"
+            binding.chipPartCheckNo.text = "계획? 나는 그런거 몰라유"
             binding.tvPartQuestion2.text = "솝트의 꽃은 앱잼! \nPM에 나갈것 같은가요?"
-            binding.btnNightCoding.text = "PM아니면 뭘 하죠?! 앱잼을 즐기려면 PM을 해야한다구욧!"
-            binding.btnCoding.text = "앱잼... 무셔... 앱잼... 몰라.... TI도 좋아......"
+            binding.chipNightCoding.text = "PM아니면 뭘 하죠?! 앱잼을 즐기려면 PM을 해야한다구욧!"
+            binding.chipCoding.text = "앱잼... 무셔... 앱잼... 몰라.... TI도 좋아......"
             part = binding.btnPlan.text.toString()
             binding.clPartQuestion.visibility = View.VISIBLE
         }
@@ -64,11 +64,11 @@ class FirstFragment : Fragment() {
             binding.tvPartMainQuestion.text = "뭐라꼬? \n니 디자이너가?"
             binding.tvPartQuestion1.text = "디자이너 하면 갬성이죠.. \n그 사람, 몽상가 재질일까요?"
             binding.tvPartQuestion1.text = "기획에는 TJ만 있을거 같죠? \n얼마나 계획적인 사람일까요?"
-            binding.btnPartCheckYes.text = "당연히 몽상을 해야 디자인을 하죠. YES!!!"
-            binding.btnPartCheckNo.text = "몽상하면 디자인이라는 고정관념을 버려주세요. NO!!!"
+            binding.chipPartCheckYes.text = "당연히 몽상을 해야 디자인을 하죠. YES!!!"
+            binding.chipPartCheckNo.text = "몽상하면 디자인이라는 고정관념을 버려주세요. NO!!!"
             binding.tvPartQuestion2.text = "인스타 한번 들어갔을 뿐인데 감성 폴폴 \n컨셉 인스타를 하고 있을까요?"
-            binding.btnNightCoding.text = "당연히 인스타는 감성을 위해 하는거 아니겠어요...?"
-            binding.btnCoding.text = "인스타는 그냥 내 마구마구 기록장! 뀨?!"
+            binding.chipNightCoding.text = "당연히 인스타는 감성을 위해 하는거 아니겠어요...?"
+            binding.chipCoding.text = "인스타는 그냥 내 마구마구 기록장! 뀨?!"
             part = binding.btnDesign.text.toString()
             binding.clPartQuestion.visibility = View.VISIBLE
         }
@@ -99,51 +99,39 @@ class FirstFragment : Fragment() {
 
         }
 
-        binding.btnOb.setOnClickListener {
-            binding.btnOb.setBackgroundResource(R.drawable.line_border_fill)
-            soptYear = binding.btnOb.text.toString()
-        }
-
-        binding.btnYb.setOnClickListener {
-            binding.btnYb.setBackgroundResource(R.drawable.line_border_fill)
-            soptYear = binding.btnYb.text.toString()
-        }
-
-        binding.btnPartCheckYes.setOnClickListener {
-            binding.btnPartCheckYes.setBackgroundResource(R.drawable.line_border_fill)
-            question1 = binding.btnPartCheckYes.text.toString()
-        }
-
-        binding.btnPartCheckNo.setOnClickListener {
-            binding.btnPartCheckNo.setBackgroundResource(R.drawable.line_border_fill)
-            question1 = binding.btnPartCheckNo.text.toString()
-        }
-
-        binding.btnNightCoding.setOnClickListener {
-            binding.btnNightCoding.setBackgroundResource(R.drawable.line_border_fill)
-            question2 = binding.btnNightCoding.text.toString()
-        }
-
-        binding.btnCoding.setOnClickListener {
-            binding.btnCoding.setBackgroundResource(R.drawable.line_border_fill)
-            question2 = binding.btnCoding.text.toString()
-        }
-
-        binding.btnAskYes.setOnClickListener {
-            binding.btnAskYes.setBackgroundResource(R.drawable.line_border_fill)
-            talkMode = true
-        }
-
-        binding.btnAskNo.setOnClickListener {
-            binding.btnAskNo.setBackgroundResource(R.drawable.line_border_fill)
-            talkMode = false
-        }
-
         binding.btnAdd.setOnClickListener {
             name = binding.etWhatName.text.toString()
             mbti = binding.etMbti.text.toString()
             face = binding.etCommonQuestion1.text.toString()
             place = binding.etCommonQuestion2.text.toString()
+
+            when (binding.chipGroupYbOb.checkedChipId) {
+                binding.chipYb.id -> soptYear = binding.chipYb.text.toString()
+                binding.chipOb.id -> soptYear = binding.chipOb.text.toString()
+                else -> Toast.makeText(requireContext(), "선택하지 않은 부분이 있습니다", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            when (binding.chipGroupPartQuestion1.checkedChipId) {
+                binding.chipPartCheckYes.id -> question1 = binding.chipPartCheckYes.text.toString()
+                binding.chipPartCheckNo.id -> question1 = binding.chipPartCheckNo.text.toString()
+                else -> Toast.makeText(requireContext(), "선택하지 않은 부분이 있습니다", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            when (binding.chipGroupPartQuestion2.checkedChipId) {
+                binding.chipNightCoding.id -> question2 = binding.chipNightCoding.text.toString()
+                binding.chipCoding.id -> question2 = binding.chipCoding.text.toString()
+                else -> Toast.makeText(requireContext(), "선택하지 않은 부분이 있습니다", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            when (binding.chipGroupTalkMode.checkedChipId) {
+                binding.chipTalkModeOn.id -> talkMode = true
+                binding.chipTalkModeOff.id -> talkMode = false
+                else -> Toast.makeText(requireContext(), "선택하지 않은 부분이 있습니다", Toast.LENGTH_SHORT)
+                    .show()
+            }
 
             val requestFriendData = RequestFriendData(
                 name = name,
@@ -157,7 +145,6 @@ class FirstFragment : Fragment() {
                 answer2 = question2,
                 ybob = soptYear,
                 tmi = tmi
-
             )
 
             val call = ServiceCreator.soptService.postAddFriend(requestFriendData)
@@ -168,7 +155,11 @@ class FirstFragment : Fragment() {
                 ) {
                     if (response.body()?.success == true) {
                         Log.d("test", "성공")
-                        Toast.makeText(requireContext(), response.body()?.message, Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            requireContext(),
+                            response.body()?.message,
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
                 }
 
@@ -176,26 +167,25 @@ class FirstFragment : Fragment() {
                     t.printStackTrace()
                     Log.d("test", "실패")
                 }
-
             })
 
-                Log.d(
-                    "test",
-                    name + " " + part + " " + soptYear + " " + mbti + " " + question1 + " " + question2 + " " + face + " " + place + " " + talkMode
-                )
+            Log.d(
+                "test",
+                name + " " + part + " " + soptYear + " " + mbti + " " + question1 + " " + question2 + " " + face + " " + place + " " + talkMode
+            )
         }
     }
-}
 
 
-private fun setScrollEvent(binding: FragmentFirstBinding) {
-    with(binding) {
-        scrollContainer.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
-            val scrollLength = scrollContainer.getChildAt(0).height - scrollContainer.height
+    private fun setScrollEvent(binding: FragmentFirstBinding) {
+        with(binding) {
+            scrollContainer.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
+                val scrollLength = scrollContainer.getChildAt(0).height - scrollContainer.height
 
-            scrollProgressBar.apply {
-                max = scrollLength
-                progress = scrollY
+                scrollProgressBar.apply {
+                    max = scrollLength
+                    progress = scrollY
+                }
             }
         }
     }
