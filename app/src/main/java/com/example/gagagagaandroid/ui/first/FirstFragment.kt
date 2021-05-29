11747994ff1a66d00e7b3948aeb3fae6a104e4b7
Leekangmin <kkk5474096@naver.com>
2021-05-29 16:03:21 +0900

    [update] FirstFragment Part 리스너 -> ChipGroup 변경

--- app/src/main/java/com/example/gagagagaandroid/ui/first/FirstFragment.kt
+++ app/src/main/java/com/example/gagagagaandroid/ui/first/FirstFragment.kt
@@ -44,59 +44,32 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         setScrollEvent(binding)
 
-        binding.btnPlan.setOnClickListener {
-            binding.btnPlan.setBackgroundResource(R.drawable.line_border_fill)
-            binding.tvPartMain.text = "기획 파트"
-            binding.tvPartMainQuestion.text = "뭐라꼬? \n니 기획자가?"
-            binding.tvPartQuestion1.text = "기획에는 TJ만 있을거 같죠? \n얼마나 계획적인 사람일까요?"
-            binding.chipPartCheckYes.text = "계획하는일이 세상에서 제일 재밌어!"
-            binding.chipPartCheckNo.text = "계획? 나는 그런거 몰라유"
-            binding.tvPartQuestion2.text = "솝트의 꽃은 앱잼! \nPM에 나갈것 같은가요?"
-            binding.chipNightCoding.text = "PM아니면 뭘 하죠?! 앱잼을 즐기려면 PM을 해야한다구욧!"
-            binding.chipCoding.text = "앱잼... 무셔... 앱잼... 몰라.... TI도 좋아......"
-            part = binding.btnPlan.text.toString()
-            binding.clPartQuestion.visibility = View.VISIBLE
-        }
 
-        binding.btnDesign.setOnClickListener {
-            binding.btnDesign.setBackgroundResource(R.drawable.line_border_fill)
-            binding.tvPartMain.text = "디자인 파트"
-            binding.tvPartMainQuestion.text = "뭐라꼬? \n니 디자이너가?"
-            binding.tvPartQuestion1.text = "디자이너 하면 갬성이죠.. \n그 사람, 몽상가 재질일까요?"
-            binding.tvPartQuestion1.text = "기획에는 TJ만 있을거 같죠? \n얼마나 계획적인 사람일까요?"
-            binding.chipPartCheckYes.text = "당연히 몽상을 해야 디자인을 하죠. YES!!!"
-            binding.chipPartCheckNo.text = "몽상하면 디자인이라는 고정관념을 버려주세요. NO!!!"
-            binding.tvPartQuestion2.text = "인스타 한번 들어갔을 뿐인데 감성 폴폴 \n컨셉 인스타를 하고 있을까요?"
-            binding.chipNightCoding.text = "당연히 인스타는 감성을 위해 하는거 아니겠어요...?"
-            binding.chipCoding.text = "인스타는 그냥 내 마구마구 기록장! 뀨?!"
-            part = binding.btnDesign.text.toString()
-            binding.clPartQuestion.visibility = View.VISIBLE
-        }
+        binding.chipGroupPart.setOnCheckedChangeListener { group, checkedId ->
+            when (checkedId) {
+                binding.chipPlan.id -> {
+                    changePlan(binding)
+                }
+                binding.chipDesign.id -> {
+                    changeDesign(binding)
+                }
 
-        binding.btnWeb.setOnClickListener {
-            binding.btnWeb.setBackgroundResource(R.drawable.line_border_fill)
-            binding.clPartQuestion.visibility = View.VISIBLE
-            part = binding.btnWeb.text.toString()
+                binding.chipIos.id  -> {
+                    changeDevelop(binding)
+                }
 
-        }
+                binding.chipAndroid.id  -> {
+                    changeDevelop(binding)
+                }
 
-        binding.btnServer.setOnClickListener {
-            binding.btnServer.setBackgroundResource(R.drawable.line_border_fill)
-            binding.clPartQuestion.visibility = View.VISIBLE
-            part = binding.btnServer.text.toString()
-        }
+                binding.chipServer.id  -> {
+                    changeDevelop(binding)
+                }
 
-        binding.btnIos.setOnClickListener {
-            binding.btnIos.setBackgroundResource(R.drawable.line_border_fill)
-            binding.clPartQuestion.visibility = View.VISIBLE
-            part = binding.btnIos.text.toString()
-        }
-
-        binding.btnAndroid.setOnClickListener {
-            binding.btnAndroid.setBackgroundResource(R.drawable.line_border_fill)
-            binding.clPartQuestion.visibility = View.VISIBLE
-            part = binding.btnAndroid.text.toString()
-
+                binding.chipWeb.id  -> {
+                    changeDevelop(binding)
+                }
+            }
         }
 
         binding.btnAdd.setOnClickListener {
@@ -188,5 +161,45 @@ class FirstFragment : Fragment() {
                 }
             }
         }
+    }
+
+    private fun changeDevelop(binding: FragmentFirstBinding) {
+        binding.tvPartMain.text = "개발 파트"
+        binding.tvPartMainQuestion.text = "뭐라꼬? \n니 개발자가?"
+        binding.tvPartQuestion1.text = "개발 실력은 체크셔츠에서 나오는거 인정?! \n체크셔츠가 옷장에 쌓여 있을까요?"
+        binding.chipPartCheckYes.text = "철이 없었죠... 체크셔츠가 많으면 코딩을 잘 할 수 있을거라 생각한게.."
+        binding.chipPartCheckNo.text = "체크셔츠는 그냥 체크셔츠일 뿐. 오해하지 말자.!"
+        binding.tvPartQuestion2.text = "해뜨면 눈감는 개발자?!\n 이 사람은 밤코일까요, 낮코일까요?\""
+        binding.chipNightCoding.text = "나는 부엉부엉 부엉이 밤코"
+        binding.chipCoding.text = "일찍 일어나는 새가 더 배부른 법이지! 나는 낮코"
+        part = binding.chipDesign.text.toString()
+        binding.clPartQuestion.visibility = View.VISIBLE
+    }
+
+    private fun changeDesign(binding: FragmentFirstBinding) {
+        binding.tvPartMain.text = "디자인 파트"
+        binding.tvPartMainQuestion.text = "뭐라꼬? \n니 디자이너가?"
+        binding.tvPartQuestion1.text = "디자이너 하면 갬성이죠.. \n그 사람, 몽상가 재질일까요?"
+        binding.tvPartQuestion1.text = "기획에는 TJ만 있을거 같죠? \n얼마나 계획적인 사람일까요?"
+        binding.chipPartCheckYes.text = "당연히 몽상을 해야 디자인을 하죠. YES!!!"
+        binding.chipPartCheckNo.text = "몽상하면 디자인이라는 고정관념을 버려주세요. NO!!!"
+        binding.tvPartQuestion2.text = "인스타 한번 들어갔을 뿐인데 감성 폴폴 \n컨셉 인스타를 하고 있을까요?"
+        binding.chipNightCoding.text = "당연히 인스타는 감성을 위해 하는거 아니겠어요...?"
+        binding.chipCoding.text = "인스타는 그냥 내 마구마구 기록장! 뀨?!"
+        part = binding.chipDesign.text.toString()
+        binding.clPartQuestion.visibility = View.VISIBLE
+    }
+
+    private fun changePlan(binding: FragmentFirstBinding) {
+        binding.tvPartMain.text = "기획 파트"
+        binding.tvPartMainQuestion.text = "뭐라꼬? \n니 기획자가?"
+        binding.tvPartQuestion1.text = "기획에는 TJ만 있을거 같죠? \n얼마나 계획적인 사람일까요?"
+        binding.chipPartCheckYes.text = "계획하는일이 세상에서 제일 재밌어!"
+        binding.chipPartCheckNo.text = "계획? 나는 그런거 몰라유"
+        binding.tvPartQuestion2.text = "솝트의 꽃은 앱잼! \nPM에 나갈것 같은가요?"
+        binding.chipNightCoding.text = "PM아니면 뭘 하죠?! 앱잼을 즐기려면 PM을 해야한다구욧!"
+        binding.chipCoding.text = "앱잼... 무셔... 앱잼... 몰라.... TI도 좋아......"
+        part = binding.chipPlan.text.toString()
+        binding.clPartQuestion.visibility = View.VISIBLE
     }
 }
